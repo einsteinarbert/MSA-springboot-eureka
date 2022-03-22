46354340bc1e6cd84d364b5f66fa332fd08e3285
Độ Nguyễn Tiến <millo@os-Mac-mini.local>
2022-03-22 18:47:00 +0700

    Done css + giao dien registration

--- Ayakashi-admin/src/main/java/com/bunbusoft/ayakashi/configuration/SecurityConfiguration.java
+++ Ayakashi-admin/src/main/java/com/bunbusoft/ayakashi/configuration/SecurityConfiguration.java
@@ -40,7 +40,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.rememberMe().key("uniqueAndSecret").tokenValiditySeconds(604800);
         http.authorizeRequests().antMatchers(
                         "/registration**",
                         "/registration/forgot-password**",
