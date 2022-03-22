26ab58b92b027e236251703b76353d2672ca1f8d
Than Trung Hieu <hieu19926@gmail.com>
2022-03-22 22:28:27 +0700

    add menu + page html

---
+++ Ayakashi-admin/src/main/java/com/bunbusoft/ayakashi/controller/WalletController.java
@@ -0,0 +1,20 @@
+package com.bunbusoft.ayakashi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "pages")
+@Slf4j
+public class WalletController {
+
+    @GetMapping(value = "/wallet/jewel-manager")
+    public ModelMap jewelProductManager() {
+        log.info("jewelProductManager()");
+        return new ModelMap();
+    }
+
+}
