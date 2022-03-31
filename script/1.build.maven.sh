3a4ba7c86c947bf4331cc4b4d8910b202a15e636
Than Trung Hieu <hieu19926@gmail.com>
2022-03-31 13:55:04 +0700

    fix docker

--- script/1.build.maven.sh
+++ script/1.build.maven.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 set -e
+cecho() {
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[1;33m"
+    # ... ADD MORE COLORS
+    NC="\033[0m" # No Color
+    # ZSH
+    # printf "${(P)1}${2} ${NC}\n"
+    # Bash
+    printf "${!1}${2} ${NC}\n"
+}
+
 cd ../
 home=`pwd`
-echo HOME: $home
+cecho "YELLOW" "HOME: $home"
 cd gateway-zuul ; mvn clean package -DskipTests; docker build -t gateway-service:1.0 .
 cd $home
-cd game-api ; mvn clean package -DskipTests; docker build -t game-api:1.0
+cd game-api ; mvn clean package -DskipTests; docker build -t game-api:1.0 .
 cd $home
 cd Ayakashi-admin ; mvn clean package -DskipTests; docker build -t admin-service:1.0 .
 cd $home
-echo "BUILD SUCCESSFULLY"
+cecho "GREEN" "BUILD ALL SUCCESSFULLY"
 
