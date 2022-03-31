2b7c519572c84efc270f21524ef0c559c23fa1e4
Ubuntu <ubuntu@ip-10-100-0-219.ap-northeast-1.compute.internal>
2022-03-31 06:37:54 +0000

    mariadb

--- script/0.maria.sh
+++ script/0.maria.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+docker rm -f db
 
-docker run --detach --name db \
+docker run --detach --name db -p 3306:3306 \
+   -v /home/ubuntu/deploy/volumes/mysql:/var/lib/mysql \
  --env MARIADB_USER=lukyluke \
  --env MARIADB_PASSWORD=lukyluke \
  --env MARIADB_ROOT_PASSWORD=lukyluke  mariadb:latest
