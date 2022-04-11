4e65ec8d51b9611989d360c6a28cb9373be63bc0
Nguyễn Tiến Độ <donguyen.pt29@amela.vn>
2022-04-11 14:10:25 +0700

    Events

---
+++ game-api/src/main/java/io/github/eureka/api/controller/EventsController.java
@@ -0,0 +1,15 @@
+package io.github.eureka.api.controller;
+
+import io.github.eureka.api.model.dto.BaseMsgDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class EventsController {
+	GetMapping("/get-events")
+	public BaseMsgDTO<Events>
+}
---
+++ game-api/src/main/java/io/github/eureka/api/service/impl/EventsServiceImpl.java
@@ -0,0 +1,8 @@
+package io.github.eureka.api.service.impl;
+
+import io.github.eureka.api.service.EventsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventsServiceImpl implements EventsService {
+}
