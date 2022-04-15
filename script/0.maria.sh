3925dbd787566b6476be2b2ef483b55085124ecc
HieuTT <ubuntu@ip-10-100-0-219.ap-northeast-1.compute.internal>
2022-04-15 10:58:40 +0000

    fix build db

--- script/0.maria.sh
+++ script/0.maria.sh
@@ -5,4 +5,5 @@ docker run --detach --name db -p 3306:3306 \
    -v /home/ubuntu/deploy/volumes/mysql:/var/lib/mysql \
  --env MARIADB_USER=lukyluke \
  --env MARIADB_PASSWORD=lukyluke \
+ --env BIND_ADDRESS=52.199.126.208 \
  --env MARIADB_ROOT_PASSWORD=lukyluke  mariadb:latest
