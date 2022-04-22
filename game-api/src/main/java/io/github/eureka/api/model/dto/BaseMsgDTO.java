18b7a63dd5947f9d65aba7642daa359ffcd688ae
Than Trung Hieu <hieu19926@gmail.com>
2022-04-22 16:22:21 +0700

    update base restponse

--- game-api/src/main/java/io/github/eureka/api/model/dto/BaseMsgDTO.java
+++ game-api/src/main/java/io/github/eureka/api/model/dto/BaseMsgDTO.java
@@ -1,7 +1,6 @@
 package io.github.eureka.api.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.github.eureka.api.common.MsgUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,25 +27,17 @@ public class BaseMsgDTO<T> implements Serializable {
     @JsonInclude()
     @Transient
     public static final String NG = "failure";
+    @Builder.Default
     private int code = 200;
+    @Builder.Default
     private String status = OK;
     private String message;
     private T data;
 
     public BaseMsgDTO(T data) {
-        this.message = "OK";
-        this.data = data;
-    }
-
-    public BaseMsgDTO(T data, int code) {
-        this.code = code;
-        this.message = MsgUtil.getMessage(String.valueOf(code));
         this.data = data;
     }
     public static <T> BaseMsgDTO<T> success(T data) {
         return new BaseMsgDTO<>(data);
-    }
-    public static <T> BaseMsgDTO<T> success(T data, int code) {
-        return new BaseMsgDTO<>(data, code);
     }
 }
