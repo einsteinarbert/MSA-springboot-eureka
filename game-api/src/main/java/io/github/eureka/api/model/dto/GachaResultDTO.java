08d9da1f4682dfd518f4bf6ceb7c80294b51c768
Nguyễn Tiến Độ <donguyen.pt29@amela.vn>
2022-04-25 16:49:45 +0700

    validation spin gacha

--- game-api/src/main/java/io/github/eureka/api/model/dto/GachaResultDTO.java
+++ game-api/src/main/java/io/github/eureka/api/model/dto/GachaResultDTO.java
@@ -11,6 +11,8 @@ import java.sql.Timestamp;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class GachaResultDTO {
 	private Long id;
 	private Long gachaId;
@@ -22,102 +24,6 @@ public class GachaResultDTO {
 	private String memo;
 	private Timestamp createdAt;
 	private Timestamp updatedAt;
-	private Characters characters;
-	private SpecialItems specialItems;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getGachaId() {
-		return gachaId;
-	}
-
-	public void setGachaId(Long gachaId) {
-		this.gachaId = gachaId;
-	}
-
-	public Long getCharacterId() {
-		return characterId;
-	}
-
-	public void setCharacterId(Long characterId) {
-		this.characterId = characterId;
-	}
-
-	public Double getProbability() {
-		return probability;
-	}
-
-	public void setProbability(Double probability) {
-		this.probability = probability;
-	}
-
-	public Integer getSkillLevel() {
-		return skillLevel;
-	}
-
-	public void setSkillLevel(Integer skillLevel) {
-		this.skillLevel = skillLevel;
-	}
-
-	public String getPickup() {
-		return pickup;
-	}
-
-	public void setPickup(String pickup) {
-		this.pickup = pickup;
-	}
-
-	public String getMemo() {
-		return memo;
-	}
-
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-
-	public Timestamp getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Timestamp createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Timestamp getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Timestamp updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public Characters getCharacters() {
-		return characters;
-	}
-
-	public void setCharacters(Characters characters) {
-		this.characters = characters;
-	}
-
-	public SpecialItems getSpecialItems() {
-		return specialItems;
-	}
-
-	public void setSpecialItems(SpecialItems specialItems) {
-		this.specialItems = specialItems;
-	}
-
-	public Long getItemId() {
-		return itemId;
-	}
-
-	public void setItemId(Long itemId) {
-		this.itemId = itemId;
-	}
+	private Characters character;
+	private SpecialItems specialItem;
 }
