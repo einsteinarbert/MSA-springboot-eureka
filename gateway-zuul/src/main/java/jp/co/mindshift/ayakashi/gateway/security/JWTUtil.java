c5984c33d1162b54c03bd9a57fe0defcb82973dc
Than Trung Hieu <hieu19926@gmail.com>
2022-05-10 16:27:33 +0700

    fix time token

--- gateway-zuul/src/main/java/jp/co/mindshift/ayakashi/gateway/security/JWTUtil.java
+++ gateway-zuul/src/main/java/jp/co/mindshift/ayakashi/gateway/security/JWTUtil.java
@@ -1,10 +1,9 @@
 package jp.co.mindshift.ayakashi.gateway.security;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import jp.co.mindshift.ayakashi.gateway.model.Users;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jp.co.mindshift.ayakashi.gateway.model.Users;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
